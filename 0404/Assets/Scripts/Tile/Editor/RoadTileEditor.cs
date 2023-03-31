eeeb2bf9ff9b8bcbad63e83ac6412ff2bfb19f12
junho_Jeon <115610258+Pigon5@users.noreply.github.com>
2023-03-31 13:11:59 +0900

    수업 다 따라가기

--- 0404/Assets/Scripts/Tile/Editor/RoadTileEditor.cs
+++ 0404/Assets/Scripts/Tile/Editor/RoadTileEditor.cs
@@ -7,7 +7,7 @@ using UnityEditor;
 
 // RoadTile용 인스펙터 변경해주는 클래스
 [CustomEditor(typeof(RoadTile))]
-public class RoadTileEditor : Editor
++public class RoadTileEditor : Editor
 {
     RoadTile roadTile;
 
@@ -25,12 +25,12 @@ public class RoadTileEditor : Editor
     {
         base.OnInspectorGUI();  // 기본적으로 그리던 것은 계속 그리기
 
-        if( roadTile != null && roadTile.sprites != null )  // roadTile과 roadTile.sprites가 있을 때만
-        {   
+        if (roadTile != null && roadTile.sprites != null)  // roadTile과 roadTile.sprites가 있을 때만
+        {
             EditorGUILayout.LabelField("Sprites Preview");  // 중간 제목 추가
             GUILayout.BeginHorizontal();                    // 수평으로 그린다고 표시
-            Texture2D texture;                              
-            foreach(var sprite in roadTile.sprites )        // roadTile의 sprites에 대해
+            Texture2D texture;
+            foreach (var sprite in roadTile.sprites)        // roadTile의 sprites에 대해
             {
                 texture = AssetPreview.GetAssetPreview(sprite); // 스프라이트의 프리뷰 이미지 가져와서 texture에 넣기
                 GUILayout.Label("", GUILayout.Height(64), GUILayout.Width(64)); // 칸 확보하기
@@ -41,5 +41,3 @@ public class RoadTileEditor : Editor
     }
 }
 #endif
-
-
--- 0404/Assets/Scripts/Tile/RoadTile.cs
+++ 0404/Assets/Scripts/Tile/RoadTile.cs
@@ -14,25 +14,26 @@ using UnityEditor;
 
 public class RoadTile : Tile
 {
-    [Flags]
-
-    enum AdjTilePosition : byte
+    /// <summary>
+    /// 주변 어느 위치에 RoadTile이 있는지 표시해주는 enum
+    /// </summary>
+    [Flags]         // 이 enum은 비트플래그로 사용하겠다라는 어트리뷰트
+    enum AdjTilePosition : byte         // 이 enum은 byte(8bit) 크기를 가진다.
     {
-        None = 0,               //0000 0000
-        North = 1,              //0000 0001
-        East = 2,               //0000 0010
-        South = 4,              //0000 0100
-        West = 8,               //0000 1000
-        All = North| East | South | West ////0000 1111
-
+        None = 0,       // 0000 0000
+        North = 1,      // 0000 0001
+        East = 2,       // 0000 0010
+        South = 4,      // 0000 0100
+        West = 8,       // 0000 1000
+        All = North | East | South | West    // 0000 1111
     }
-    public Sprite[] sprites;
 
+    public Sprite[] sprites;
 
     /// <summary>
     /// 타일이 그려질 때 자동으로 호출이 되는 함수.
     /// </summary>
-    /// <param name="position">타일의 위치 그리드의 좌표값</param>
+    /// <param name="position">타일의 위치(그리드의 좌표값)</param>
     /// <param name="tilemap">이 타일이 그려지는 타일맵</param>
     public override void RefreshTile(Vector3Int position, ITilemap tilemap)
     {
@@ -40,58 +41,56 @@ public class RoadTile : Tile
         {
             for (int x = -1; x < 2; x++)
             {
-                Vector3Int location = new(position.x + x, position.y + y, position.z);
-                if (HasThisTile(tilemap, location))
+                Vector3Int location = new(position.x + x, position.y + y, position.z); // 나와 주변타일의 위치
+                if (HasThisTile(tilemap, location))  // 같은 종류의 타일이면
                 {
-                    tilemap.RefreshTile(location);
+                    tilemap.RefreshTile(location);  // 갱신시키기(보이는 이미지 변경)
                 }
             }
         }
     }
 
-
-
-
     /// <summary>
-    /// 타일이 실제로 어떤 스트라이트를 그리는지 결정하는 함수.
+    /// 타일이 실제로 어떤 스프라이트를 그리는지 결정하는 함수
     /// (tileData에 그려질 타일의 정보를 넘겨준다.)
     /// </summary>
     /// <param name="position">타일 데이터를 가져올 타일의 위치</param>
     /// <param name="tilemap">타일 데이터를 가져올 타일맵</param>
-    /// <param name="tileData">가져온 타일 데이터의 참조(읽기,쓰기 가능 )</param>
+    /// <param name="tileData">가져온 타일 데이터의 참조(읽기, 쓰기 가능)</param>
     public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
     {
         AdjTilePosition mask = AdjTilePosition.None;
 
-        base.GetTileData(position, tilemap, ref tileData);
+        // mask에 주변 타일 정보 기록
         mask |= HasThisTile(tilemap, position + new Vector3Int(0, 1, 0)) ? AdjTilePosition.North : 0;
+        //if(HasThisTile(tilemap, position + new Vector3Int(0, 1, 0)))
+        //{
+        //    mask = mask | AdjTilePosition.North;
+        //}
+        mask |= HasThisTile(tilemap, position + new Vector3Int(1, 0, 0)) ? AdjTilePosition.East : 0;
+        mask |= HasThisTile(tilemap, position + new Vector3Int(0, -1, 0)) ? AdjTilePosition.South : 0;
+        mask |= HasThisTile(tilemap, position + new Vector3Int(-1, 0, 0)) ? AdjTilePosition.West : 0;
 
-        mask |= HasThisTile(tilemap,position + new Vector3Int(1, 0, 0)) ? AdjTilePosition.East : 0;
-
-        mask |= HasThisTile(tilemap,position + new Vector3Int(0, -1, 0)) ? AdjTilePosition.South : 0;
-        mask |= HasThisTile(tilemap,position + new Vector3Int(1, 0, 0)) ? AdjTilePosition.West : 0;
-
-        int index = GetIndex(mask);
-        if(index > - 1) 
+        int index = GetIndex(mask); // 어떤 스프라이트를 그릴 것인지 결정
+        if (index > -1)
         {
-            tileData.sprite = sprites[index];
-            tileData.color = Color.white;
+            tileData.sprite = sprites[index];           // 스프라이트 적용
+            tileData.color = Color.white;               // 색상을 기본인 흰색으로 설정
             Matrix4x4 m = tileData.transform;
-            m.SetTRS(Vector3.zero, GetRotation(mask), Vector3.one);
+            m.SetTRS(Vector3.zero, GetRotation(mask), Vector3.one); // 회전 적용하기
             tileData.transform = m;
-            tileData.flags = TileFlags.LockTransform;
-            tileData.colliderType = ColliderType.None;
+            tileData.flags = TileFlags.LockTransform;   // 다른 타일이 회전을 못시키게 만들기
+            tileData.colliderType = ColliderType.None;  // 컬라이더 없는 것으로 처리
         }
         else
         {
-            Debug.LogError($"잘못된 인덱스: {index}");
+            Debug.LogError($"잘못된 인덱스 : {index}, mask = {mask}");
         }
-    
     }
 
     private int GetIndex(AdjTilePosition mask)
     {
-        int  index = -1;
+        int index = -1;
 
         switch (mask)
         {
@@ -102,22 +101,22 @@ public class RoadTile : Tile
             case AdjTilePosition.West:
             case AdjTilePosition.East | AdjTilePosition.West:
             case AdjTilePosition.North | AdjTilePosition.South:
-                index = 0;                  //1자 모양의 스프라이트
+                index = 0;      // l자 모양의 스프라이트
                 break;
             case AdjTilePosition.South | AdjTilePosition.West:
             case AdjTilePosition.West | AdjTilePosition.North:
             case AdjTilePosition.North | AdjTilePosition.East:
             case AdjTilePosition.East | AdjTilePosition.South:
-                index = 1;                  //ㄱ자 모양의 스프라이트
+                index = 1;      // ㄱ자 모양의 스프라이트
                 break;
-            case AdjTilePosition.All & ~AdjTilePosition.North: //0000 1111 & ~0000 0001 = 0000 1111 & 1111 111-0 = 0000 1110 
+            case AdjTilePosition.All & ~AdjTilePosition.North:  // 0000 1111 & ~0000 0001 = 0000 1111 & 1111 1110 = 0000 1110
             case AdjTilePosition.All & ~AdjTilePosition.East:
             case AdjTilePosition.All & ~AdjTilePosition.South:
             case AdjTilePosition.All & ~AdjTilePosition.West:
-                index = 2;                  //ㅗ자 모양의 스프라이트
+                index = 2;      // ㅗ자 모양의 스프라이트
                 break;
             case AdjTilePosition.All:
-                index = 3;                  //+자 모양의 스프라이트
+                index = 3;      // +자 모양의 스프라이트
                 break;
         }
 
@@ -128,68 +127,62 @@ public class RoadTile : Tile
     {
         Quaternion rotate = Quaternion.identity;
 
-        //기본 : 1, ㄱ, ㅗ 형태 
+        // 기본 : l, ㄱ, ㅗ
         switch (mask)
         {
-            case AdjTilePosition.East:                               //1자
-            case AdjTilePosition.West:                      
-            case AdjTilePosition.East | AdjTilePosition.West:        //ㄱ2자
-            case AdjTilePosition.West | AdjTilePosition.North:
-            case AdjTilePosition.All & ~AdjTilePosition.West:       //ㅗ자
+            case AdjTilePosition.East:                          // l자
+            case AdjTilePosition.West:
+            case AdjTilePosition.East | AdjTilePosition.West:
+            case AdjTilePosition.West | AdjTilePosition.North:  // ㄱ자
+            case AdjTilePosition.All & ~AdjTilePosition.West:   // ㅗ자
                 rotate = Quaternion.Euler(0, 0, -90);
                 break;
-            case AdjTilePosition.North | AdjTilePosition.East:      //ㄱ
-            case AdjTilePosition.All & ~AdjTilePosition.North:      //ㅗ
+            case AdjTilePosition.North | AdjTilePosition.East:  // ㄱ자
+            case AdjTilePosition.All & ~AdjTilePosition.North:  // ㅗ자
                 rotate = Quaternion.Euler(0, 0, -180);
                 break;
-            case AdjTilePosition.East | AdjTilePosition.South:
-            case AdjTilePosition.All & ~AdjTilePosition.East:
+            case AdjTilePosition.East | AdjTilePosition.South:  // ㄱ자
+            case AdjTilePosition.All & ~AdjTilePosition.East:   // ㅗ자
                 rotate = Quaternion.Euler(0, 0, -270);
                 break;
         }
+
         return rotate;
     }
 
 
     /// <summary>
-    /// 타일맵에서 지정된 위이에 있는 타일이 같은 종류의 타일인지 확인하는 함수
+    /// 타일맵에서 지정된 위치에 있는 타일이 같은 종류의 타일인지 확인하는 함수
     /// </summary>
     /// <param name="tilemap">확인할 타일맵</param>
-    /// <param name="position">true면 같은 종류의 타일. false면 다른 종류의 타일</param>
-    /// <returns></returns>
+    /// <param name="position">확인할 위치</param>
+    /// <returns>true면 같은 종류의 타일. false면 다른 종류의 타일</returns>
     bool HasThisTile(ITilemap tilemap, Vector3Int position)
     {
-
-        return tilemap.GetTile(position) == this;   //타일은 1개 타일맵은 타일의 정보를 참조해서 모여주기 때문에 
+        return tilemap.GetTile(position) == this;   // 타일은 1개. 타일맵은 타일의 정보를 참조해서 보여주기 때문에 이 코드가 가능
     }
 
 #if UNITY_EDITOR
     [MenuItem("Assets/Create/2D/Tiles/RoadTile")]
     public static void CreateRoadTile()
     {
-        string path = EditorUtility.SaveFilePanelInProject( //파일 저장용 창 열기 
-            "Save Road Tile",                               //제목
-            "New Road Tile",                                //파일의 기본 이름 
-            "Asset",                                        //파일의 확장자
-            "Save Road Tile",                               //출력되는 메세지
-            "Assets");                                      //열리는 기본 폴더
-        
-        if(path != string.Empty )                           //path가 비어있지 않다면
+        string path = EditorUtility.SaveFilePanelInProject( // 파일 저장용 창 열기
+            "Save Road Tile",   // 제목
+            "New Road Tile",    // 파일의 기본 이름
+            "Asset",            // 파일의 확장자
+            "Save Road Tile",   // 출력되는 메세지
+            "Assets");          // 열리는 기본 폴더
+
+        if (path != string.Empty)    // path가 비어있지 않다면
         {
-            AssetDatabase.CreateAsset(CreateInstance<RoadTile>(), path);    //RoadTile을 path위치에 생성한다.
+            AssetDatabase.CreateAsset(CreateInstance<RoadTile>(), path);    // RoadTile을 path위치에 생성
         }
-        
-    
     }
 #endif
-
-
-
-
 }
 
 
 
-    
+
 
 
